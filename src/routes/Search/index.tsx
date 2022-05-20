4af84708d10b8c5071436e5d4fc9ad2854fc5594
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-05-20 17:55:09 +0900

    Feat: add search markup
    Co-Authored-By: LUMPEN <68418005+lumpenop@users.noreply.github.com>
    Co-Authored-By: Lee Ju I <103873136+jui9266@users.noreply.github.com>
    Co-Authored-By: hsw824 <79175916+hsw824@users.noreply.github.com>

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,5 +1,32 @@
+import styles from './Search.module.scss'
+
+import { SearchIcon } from 'assets/svgs'
+
+const DUMMY_RECOMMEND = ['간세포암', '고환암', '뼈암', '구강암']
+
 const Search = () => {
-  return <div>humanscape_search_8A</div>
+  return (
+    <div className={styles.searchContainer}>
+      <p className={styles.description}>
+        국내 모든 임상시험 검색하고
+        <br /> 온라인으로 참여하기
+      </p>
+
+      <div className={styles.searchInputWarrper}>
+        <SearchIcon />
+        <input className={styles.searchInput} placeholder='질환명을 입력해 주세요.' />
+        <button type='button' className={styles.searchButton}>
+          검색
+        </button>
+      </div>
+      <ul className={styles.dropdown}>
+        <span>추천 검색어</span>
+        {DUMMY_RECOMMEND.map((dummy) => {
+          return <li key={dummy}>{dummy}</li>
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default Search
