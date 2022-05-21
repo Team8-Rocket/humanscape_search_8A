58f68309017bb397f9d668926b3913148e378546
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-05-21 17:40:19 +0900

    Feat: Add Search API
    Co-Authored-By: LUMPEN <68418005+lumpenop@users.noreply.github.com>
    Co-Authored-By: Lee Ju I <103873136+jui9266@users.noreply.github.com>
    Co-Authored-By: hsw824 <79175916+hsw824@users.noreply.github.com>

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,30 +1,84 @@
+import { ChangeEvent, KeyboardEvent } from 'react'
+import { useQuery } from 'react-query'
+import { useEffect, useRef, useAppSelector, useQueryDebounce, useAppDispatch, useState } from 'hooks'
+import { getDiseasesName } from 'services/search'
+
 import styles from './Search.module.scss'
-
 import { SearchIcon } from 'assets/svgs'
-
-const DUMMY_RECOMMEND = ['간세포암', '고환암', '뼈암', '구강암']
+import List from './List'
+import { setActiveIndex, setSearchText } from 'states/search'
 
 const Search = () => {
+  const debouncedSearchText = useAppSelector((state) => state.disease.debouncedText)
+  const activeIndex = useAppSelector((state) => state.disease.activeIndex)
+  const searchText = useAppSelector((state) => state.disease.searchText)
+
+  const dispatch = useAppDispatch()
+
+  const [searchCount, setSearchCount] = useState<number>(0)
+
+  const ref = useRef<HTMLInputElement>(null)
+  useQueryDebounce(searchText.replaceAll(' ', ''))
+
+  const { data, isLoading } = useQuery(
+    ['diseaseList', debouncedSearchText],
+    () => getDiseasesName(debouncedSearchText, searchCount, setSearchCount),
+
+    {
+      enabled: debouncedSearchText !== '',
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5,
+    }
+  )
+
+  const handleChangeSearchText = (evt: ChangeEvent<HTMLInputElement>) => {
+    const { value } = evt.currentTarget
+    dispatch(setSearchText(value))
+  }
+
+  const handleKeyMove = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (!data || event.nativeEvent.isComposing) return
+
+    if (event.key === 'Enter') {
+      dispatch(setSearchText(searchText))
+      return
+    }
+    if (event.key === 'ArrowUp')
+      dispatch(activeIndex > 0 ? setActiveIndex(activeIndex - 1) : setActiveIndex(data.length - 1))
+
+    if (event.key === 'ArrowDown')
+      dispatch(activeIndex === data.length - 1 ? setActiveIndex(0) : setActiveIndex(activeIndex + 1))
+  }
+
+  useEffect(() => {
+    dispatch(setActiveIndex(-1))
+    ref.current?.focus()
+  }, [searchText, dispatch])
+
   return (
     <div className={styles.searchContainer}>
       <p className={styles.description}>
         국내 모든 임상시험 검색하고
-        <br /> 온라인으로 참여하기
+        <br />
+        온라인으로 참여하기
       </p>
 
       <div className={styles.searchInputWarrper}>
-        <SearchIcon />
-        <input className={styles.searchInput} placeholder='질환명을 입력해 주세요.' />
+        <SearchIcon className={styles.searchIcon} />
+        <input
+          className={styles.searchInput}
+          value={searchText}
+          onChange={handleChangeSearchText}
+          placeholder='질환명을 입력해 주세요.'
+          onKeyDown={handleKeyMove}
+          ref={ref}
+        />
         <button type='button' className={styles.searchButton}>
           검색
         </button>
       </div>
-      <ul className={styles.dropdown}>
-        <span>추천 검색어</span>
-        {DUMMY_RECOMMEND.map((dummy) => {
-          return <li key={dummy}>{dummy}</li>
-        })}
-      </ul>
+
+      {debouncedSearchText && <List data={data} isLoading={isLoading} />}
     </div>
   )
 }
