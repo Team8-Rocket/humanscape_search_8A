3f61cf339503886d6812710cafe4b55a2ffe33ee
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-05-21 17:39:23 +0900

    Delete: 404 page
    Co-Authored-By: LUMPEN <68418005+lumpenop@users.noreply.github.com>
    Co-Authored-By: Lee Ju I <103873136+jui9266@users.noreply.github.com>
    Co-Authored-By: hsw824 <79175916+hsw824@users.noreply.github.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
-
-import Search from './Search'
-
 import styles from './Routes.module.scss'
+
+import Search from 'routes/Search'
 
 const App = () => {
   return (
-    <div className={styles.app}>
-      <Routes>
-        <Route path='/' element={<Search />} />
-        <Route path='*' element={<div>Not Found</div>} />
-      </Routes>
+    <div className={styles.appWrapper}>
+      <div className={styles.app}>
+        <Routes>
+          <Route path='/' element={<Search />} />
+        </Routes>
+      </div>
     </div>
   )
 }
